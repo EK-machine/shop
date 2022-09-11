612b19f47b39fef7c22753040d3e10f3e1cf3896
EK-machine <57370574+EK-machine@users.noreply.github.com>
2022-09-11 15:07:48 +0300

    removed homepage

--- src/components/HomePage/HomePage.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import styles from './style.module.css';
-
-const HomePage: React.FC = () => <div className={styles.homeContainer}>HomePage</div>;
-
-export default HomePage;
