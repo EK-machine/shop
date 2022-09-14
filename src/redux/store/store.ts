9578d4a36c9c184bee982ffe0122cc2394fb8dc2
EK-machine <57370574+EK-machine@users.noreply.github.com>
2022-09-14 09:04:51 +0300

    added redux-persist

--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -1,16 +1,35 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import commonReducer from '../slices/commonStateSlice';
 import userProfileReducer from '../slices/userProfileSlice';
 import modalContentReducer from '../slices/modalContentSlice';
 import allProductsReducer from '../slices/allProductsSlice';
 
-const store = configureStore({
-  reducer: {
-    common: commonReducer,
-    user: userProfileReducer,
-    modal: modalContentReducer,
-    products: allProductsReducer,
-  },
+const rootReducer = combineReducers({
+  common: commonReducer,
+  user: userProfileReducer,
+  modal: modalContentReducer,
+  products: allProductsReducer,
 });
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
 
 export default store;
